673ddaaddee551441f586d2ad8471a304b0eecdb
Anthony Martinez <anthony.mart996@gmail.com>
2024-07-26 23:15:45 -0400

    set providers for typeorm entities in users controller test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Agent } from 'src/sales/models/agent.entity';
+import { Customer } from 'src/sales/models/customer.entity';
+import { Order } from 'src/sales/models/order.entity';
+import { User } from './models/user.entity';
+import { Role } from 'src/auth/models/role.entity';
+import { Permission } from 'src/auth/models/permission.entity';
+import { Action } from 'src/auth/models/action.entity';
+import { Resource } from 'src/auth/models/resource.entity';
+import { UsersService } from './services/users.service';
+import { Repository } from 'typeorm';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  const entities = [
+    User,
+    Agent,
+    Customer,
+    Order,
+    Role,
+    Permission,
+    Action,
+    Resource,
+  ];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        ...entities.map((entity) => ({
+          provide: getRepositoryToken(entity),
+          useClass: Repository,
+        })),
+        UsersService,
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
